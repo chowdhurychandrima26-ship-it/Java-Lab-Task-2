baa182c3c65f9448fce7a0192641f9cc374adea5
Chandrima Chowdhury <chowdhurychandrima26@gmail.com>
2026-08-13 17:08:52 +0600

    Create Experiment14.java

---
+++ OOP Java Basic Programming Lab 2/Experiment14.java
@@ -0,0 +1,16 @@
+public class Problem14_AmericanFlag {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 9; i++) {
+            if (i % 2 != 0) {
+                System.out.println("* * * * * * ==================================");
+            } else {
+                System.out.println(" * * * * * ==================================");
+            }
+        }
+
+        for (int i = 1; i <= 6; i++) {
+            System.out.println("==============================================");
+        }
+    }
+}
