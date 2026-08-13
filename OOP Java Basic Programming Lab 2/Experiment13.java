9cc72f9b024b5eb4f8066aaca9c61e2eeaad680b
Chandrima Chowdhury <chowdhurychandrima26@gmail.com>
2026-08-13 17:05:45 +0600

    Create Experiment13.java

---
+++ OOP Java Basic Programming Lab 2/Experiment13.java
@@ -0,0 +1,14 @@
+public class Experiment13 {
+    public static void main(String[] args) {
+
+        double width = 5.6;
+        double height = 8.5;
+
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+
+        System.out.printf("Area is %.1f * %.1f = %.2f%n", width, height, area);
+        System.out.printf("Perimeter is 2 * (%.1f + %.1f) = %.2f%n",
+                width, height, perimeter);
+    }
+}
